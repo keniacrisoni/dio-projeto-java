4c3142646dfc731f12cb999c496aa487ae27a9ef
Kenia Crisoni <keniafcrisoni@gmail.com>
2024-02-19 22:03:09 -0300

    Conta banco aberta com sucesso

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        {
+            System.out.println("Por favor digite o número da Agência: ");
+            String agencia = sc.next();
+
+            System.out.println("Por favor digite o número da conta corrente:  ");
+            int conta = sc.nextInt();
+
+            System.out.println("Por favor digite o nome do cliente:  ");
+            String nome = sc.next();
+
+            System.out.println("Por favor digite o saldo:  ");
+            Double saldo = sc.nextDouble();
+
+            System.out.printf("""
+                    Olá %s, obrigado por criar uma conta em nosso banco, sua
+                    Agência é %s, conta %d e seu saldo %.2f, já esta disponível para saque.
+                    """, nome, agencia, conta, saldo);
+            sc.close();
+        }
+
     }
+
 }
